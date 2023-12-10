5db4a9301e34378f9ecef55dadc8c6c0db5dca86
ojela028xamk <jerelans91@gmail.com>
2023-12-10 18:12:47 +0200

    some prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 const App = (): JSX.Element => {
-    return (
-        <div className="App">
-            <header className="App-header">
-                Edit <code>src/App.tsx</code> and save to reload.
-            </header>
-        </div>
-    )
+  return (
+    <div className="App">
+      <header className="App-header">
+        Edit <code>src/App.tsx</code> and save to reload.
+      </header>
+    </div>
+  )
 }
 
 export default App
