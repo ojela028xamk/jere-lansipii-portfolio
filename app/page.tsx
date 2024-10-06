037ec2689241290c2299b5691a59ceda17595193
ojela028xamk <jerelans91@gmail.com>
2024-10-06 12:23:24 +0300

    next js project

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h1>Portfolio</h1>
+      </main>
+      <footer className={styles.footer}></footer>
+    </div>
+  );
+}
