682cf9a9b44d35356c8b9f2f77be800e4e0bb945
ojela028xamk <jerelans91@gmail.com>
2025-03-17 11:45:50 +0200

    contact translate

--- app/website/contact.tsx
+++ app/website/contact.tsx
@@ -1,10 +1,12 @@
+import { useTranslations } from "next-intl";
 import css from "./contact.module.scss";
 
 const Contact = (): JSX.Element => {
+  const t = useTranslations("website");
   return (
     <div className={css.contact}>
       <div className={css.contact_content}>
-        <h1>Stay Connected</h1>
+        <h1>{t("contact_header")}</h1>
         <div className={css.contact_items}>
           <span>Helsinki, Finland</span>
           <span>jerelans91@gmail.com</span>
