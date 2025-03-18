313d392eb8360fae9c7be707359041eb29e1d3e4
ojela028xamk <jerelans91@gmail.com>
2025-03-18 12:19:20 +0200

    language buttons design

--- app/website/languageSwitch.tsx
+++ app/website/languageSwitch.tsx
@@ -2,6 +2,7 @@
 import { useTransition } from "react";
 import css from "./languageSwitch.module.scss";
 import { setUserLocale } from "../i18n/locale";
+import Image from "next/image";
 
 export enum Locale {
   EN = "en",
@@ -22,8 +23,22 @@ const LanguageSwitch = () => {
   return (
     <div className={css.language_switch}>
       {Object.values(Locale).map((value) => (
-        <button key={value} onClick={() => handleLanguageSwitch(value)}>
-          {value}
+        <button
+          key={value}
+          className={css.language_switch_button}
+          onClick={() => handleLanguageSwitch(value)}
+        >
+          <span className={css.language_value}>
+            {value.toLocaleUpperCase()}
+          </span>
+          <div className={css.image_container}>
+            <Image
+              className={css.image}
+              src={`/${value}.png`}
+              alt={"Flag icon"}
+              fill
+            />
+          </div>
         </button>
       ))}
     </div>
