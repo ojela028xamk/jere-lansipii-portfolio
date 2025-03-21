4d64decc0acfeae0de84516b446616943e850ac3
ojela028xamk <jerelans91@gmail.com>
2025-03-21 17:56:37 +0200

    use instead of import

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,11 @@ import Contact from "./website/contact";
 import Header from "./website/header";
 import Projects from "./website/projects";
 import Skills from "./website/skills";
-import css from "./page.module.scss";
 import Experience from "./website/experience";
 
 export default function Home() {
   return (
-    <main className={css.main}>
+    <main>
       <Header />
       <Experience />
       <Skills />
