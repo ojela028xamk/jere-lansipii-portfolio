1f8d8981da0f46387299865f62c8188ae6e7c5cb
ojela028xamk <jerelans91@gmail.com>
2025-06-03 18:33:55 +0300

    modal cleanup

--- app/website/projectsModal.tsx
+++ app/website/projectsModal.tsx
@@ -5,48 +5,19 @@ export default function ProjectsModal() {
   return (
     <Dialog.Root>
       <Dialog.Trigger asChild>
-        <button className={`${css.Button} violet`}>Edit profile</button>
+        <button className={css.modal_button}>More Info</button>
       </Dialog.Trigger>
       <Dialog.Portal>
-        <Dialog.Overlay className={css.Overlay} />
-        <Dialog.Content className={css.Content}>
-          <Dialog.Title className={css.Title}>Edit profile</Dialog.Title>
-          <Dialog.Description className={css.Description}>
-            Make changes to your profile here. Click save when you're done.
+        <Dialog.Overlay className={css.modal_overlay} />
+        <Dialog.Content className={css.modal_content}>
+          <Dialog.Title className={css.modal_title}>
+            Sivuston Otsikko
+          </Dialog.Title>
+          <Dialog.Description className={css.modal_description}>
+            Kuvaus tulee tähän.
           </Dialog.Description>
-          <fieldset className={css.Fieldset}>
-            <label className={css.Label} htmlFor="name">
-              Name
-            </label>
-            <input
-              className={css.Input}
-              id="name"
-              defaultValue="Pedro Duarte"
-            />
-          </fieldset>
-          <fieldset className={css.Fieldset}>
-            <label className={css.Label} htmlFor="username">
-              Username
-            </label>
-            <input
-              className={css.Input}
-              id="username"
-              defaultValue="@peduarte"
-            />
-          </fieldset>
-          <div
-            style={{
-              display: "flex",
-              marginTop: 25,
-              justifyContent: "flex-end",
-            }}
-          >
-            <Dialog.Close asChild>
-              <button className={`${css.Button} green`}>Save changes</button>
-            </Dialog.Close>
-          </div>
           <Dialog.Close asChild>
-            <button className={css.IconButton} aria-label="Close">
+            <button className={css.modal_icon_button} aria-label="Close">
               X
             </button>
           </Dialog.Close>
