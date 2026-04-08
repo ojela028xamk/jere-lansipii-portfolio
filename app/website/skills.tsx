81b8e44878d634acd0b4edbedf73e6e21cdebb18
ojela028xamk <jerelans91@gmail.com>
2026-04-08 16:34:07 +0300

    skill translates

--- app/website/skills.tsx
+++ app/website/skills.tsx
@@ -1,63 +1,91 @@
 import css from "./skills.module.scss";
-import { FaDatabase, FaCode } from "react-icons/fa6";
+import { FaDatabase, FaCode, FaRobot } from "react-icons/fa6";
 import { useTranslations } from "next-intl";
+import { FaPencilAlt } from "react-icons/fa";
 
+type Solution = {
+  title: string;
+  description: string;
+  icon: JSX.Element;
+  skills: string[];
+};
+
+const solutions: Solution[] = [
+  {
+    title: "skills.solution1.title",
+    description: "skills.solution1.description",
+    icon: <FaCode className={css.icon} />,
+    skills: [
+      "skills.skill.html",
+      "skills.skill.javascript",
+      "skills.skill.react",
+      "skills.skill.responsive",
+      "skills.skill.github",
+      "skills.skill.api",
+    ],
+  },
+  {
+    title: "skills.solution2.title",
+    description: "skills.solution2.description",
+    icon: <FaDatabase className={css.icon} />,
+    skills: [
+      "skills.skill.excel",
+      "skills.skill.power_bi",
+      "skills.skill.sql",
+      "skills.skill.pandas",
+      "skills.skill.databricks",
+      "skills.skill.bigquery",
+      "skills.skill.google_analytics",
+      "skills.skill.google_tag",
+      "skills.skill.powerpoint",
+    ],
+  },
+  {
+    title: "skills.solution3.title",
+    description: "skills.solution3.description",
+    icon: <FaPencilAlt className={css.icon} />,
+    skills: [
+      "skills.skill.graphic",
+      "skills.skill.ui",
+      "skills.skill.figma",
+      "skills.skill.canva",
+      "skills.skill.capcut",
+    ],
+  },
+  {
+    title: "skills.solution4.title",
+    description: "skills.solution4.description",
+    icon: <FaRobot className={css.icon} />,
+    skills: [
+      "skills.skill.gemini",
+      "skills.skill.vertex",
+      "skills.skill.windsurf",
+      "skills.skill.builder",
+    ],
+  },
+];
 const Skills = (): JSX.Element => {
   const t = useTranslations("website");
-  const dataSkills = [
-    "excel",
-    "power_bi",
-    "sql",
-    "python",
-    "databricks",
-    "bigquery",
-    "google_analytics",
-    "google_tag",
-    "powerpoint",
-    "data_storytelling",
-    "data_visualization",
-  ];
-  const appSkills = [
-    "html",
-    "javascript",
-    "react",
-    "ui",
-    "graphic",
-    "responsive",
-    "figma",
-    "canva",
-    "capcut",
-    "github",
-    "api",
-  ];
 
   return (
     <div className={css.skills}>
       <div className={css.skills_content}>
         <h1>{t("skills.header")}</h1>
         <div className={css.skill_items}>
-          <div className={css.item}>
-            <div className={css.item_header}>
-              <FaCode className={css.icon} />
-              <span className={css.category}>{t("skills.app_header")}</span>
+          {solutions.map((solution) => (
+            <div key={solution.title} className={css.item}>
+              <div className={css.item_header}>
+                {solution.icon}
+                <span className={css.category}>{t(solution.title)}</span>
+              </div>
+              <p>{t(solution.description)}</p>
+              <ul>
+                {solution.skills.map((skill) => (
+                  <li key={skill}>{t(skill)}</li>
+                ))}
+              </ul>
             </div>
-            <ul>
-              {appSkills.map((skill) => (
-                <li key={skill}>{t(`skills.app_skills.${skill}`)}</li>
-              ))}
-            </ul>
-          </div>
-          <div className={css.item}>
-            <div className={css.item_header}>
-              <FaDatabase className={css.icon} />
-              <span className={css.category}>{t("skills.data_header")}</span>
-            </div>
-            <ul>
-              {dataSkills.map((skill) => (
-                <li key={skill}>{t(`skills.data_skills.${skill}`)}</li>
-              ))}
-            </ul>
-          </div>
+          ))}
         </div>
       </div>
     </div>
